6d0a773eab27da6d1f8ac92db3c548518267aba3
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2020-03-08 15:29:52 +0100

    feat: create simple index page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import ExpensesForm from '../components/ExpensesForm';
+import ExpensesTable from '../components/ExpensesTable';
+
+export default function Index() {
+  return (
+    <div>
+      <ExpensesForm />
+
+      <ExpensesTable />
+    </div>
+  );
+}
