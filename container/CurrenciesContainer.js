da1288e3181337c69c2820908c961ea6deadd5e9
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2020-03-08 15:24:19 +0100

    feat: added currencies container

---
+++ container/CurrenciesContainer.js
@@ -0,0 +1,29 @@
+import { Container } from 'unstated';
+import 'cross-fetch/polyfill';
+
+export default class CurrenciesContainer extends Container {
+  state = {
+    loading: false,
+    currencies: [],
+  }
+
+  constructor() {
+    super()
+
+    this.loadCurrencies();
+  }
+
+  async loadCurrencies() {
+    try {
+      const response = await fetch('https://openexchangerates.org/api/currencies.json');
+      const data = await response.json();
+      const currencies = Object.keys(data).map(code => ({ code, name: data[code] })).sort();
+
+      this.setState({
+        currencies,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
