5c586bb9ae5c2a55155205aa07fd43b065128e52
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2020-03-08 15:45:07 +0100

    feat: created simple table markup

--- components/ExpensesTable.js
+++ components/ExpensesTable.js
@@ -5,7 +5,31 @@ import ExpensesContainer from '../container/ExpensesContainer'
 const ExpensesTable = () => (
   <Subscribe to={[ExpensesContainer]}>
     {(expenses) => {
-      return (<table></table>)
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>Recipient</th>
+              <th>Type</th>
+              <th>Amount</th>
+              <th>Currency</th>
+              <th>Transaction Date</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {Array.isArray(expenses.state.expenses) && expenses.state.expenses.map(({ recipient, type, amount, currency, transaction_date}) => (
+              <tr>
+                <td>{recipient}</td>
+                <td>{type}</td>
+                <td>{amount}</td>
+                <td>{currency}</td>
+                <td>{transaction_date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )
     }}
   </Subscribe>
 );
