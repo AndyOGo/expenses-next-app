f2269fec5c4a3e5f409652281b96c3571499879a
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2020-03-11 21:12:45 +0100

    feat: start adding bootstrap form

---
+++ components/bootstrap/BSForm/BSForm.js
@@ -0,0 +1,7 @@
+import BSForm from 'react-bootstrap/Form';
+import BSFormLabel from 'react-bootstrap/FormLabel';
+import BSFormControl from 'react-bootstrap/FormControl';
+import './BSForm.scss';
+
+export { BSFormLabel, BSFormControl };
+export default BSForm;
