83cb47237cdbdf2098d69508a9c167fc1b24ff79
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2020-03-11 22:59:26 +0100

    feat: use bs navbar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import BSNavbar from '../components/bootstrap/BSNavbar/BSNavbar';
 import ExpensesForm from '../components/expenses/ExpensesForm';
 import ExpensesTable from '../components/expenses/ExpensesTable';
 
 export default function Index() {
   return (
     <div>
+      <BSNavbar bg="dark" variant="dark">
+        <BSNavbar.Brand>Expenses</BSNavbar.Brand>
+      </BSNavbar>
+
       <ExpensesForm />
 
       <ExpensesTable />
