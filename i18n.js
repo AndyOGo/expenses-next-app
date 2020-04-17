d210de10c89cae8b65b347929775a13bbe445e97
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2020-04-17 20:33:55 +0200

    feat: enable locale sub paths

--- i18n.js
+++ i18n.js
@@ -3,6 +3,9 @@ import NextI18Next from 'next-i18next';
 const NextI18NextInstance = new NextI18Next({
   defaultLanguage: 'en',
   otherLanguages: ['de'],
+  localeSubpaths: {
+    de: 'de',
+  },
 });
 
 export default NextI18NextInstance;
