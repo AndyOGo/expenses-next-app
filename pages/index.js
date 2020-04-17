d8ecc8d0494e5ff1152e52e589f17c3d1523d15b
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2020-04-17 21:35:30 +0200

    feat: start implementing language switch

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 
-import { i18n, Link, withTranslation } from '../i18n';
+import { i18n, withTranslation } from '../i18n';
 import BSNavbar from '../components/bootstrap/BSNavbar/BSNavbar';
 import BSNav from '../components/bootstrap/BSNav/BSNav';
 import BSCard from '../components/bootstrap/BSCard/BSCard';
+import BSNavDropdown from '../components/bootstrap/BSNavDropdown/BSNavDropdown';
 import BSOverlayTrigger from '../components/bootstrap/BSOverlayTrigger/BSOverlayTrigger';
 import BSTooltip from '../components/bootstrap/BSTooltip/BSTooltip';
 import BSScreenReaders from '../components/bootstrap/BSScreenReaders/BSScreenReaders';
@@ -12,11 +14,28 @@ import Notifications from '../components/expenses/Notifications';
 import ExpensesForm from '../components/expenses/ExpensesForm';
 import ExpensesTable from '../components/expenses/ExpensesTable';
 
+const BSDropdownItemLink = ({ href, ...props }) => (
+  <Link href={href}>
+    <a {...props} />
+  </Link>
+);
+
 function Index({ t }) {
   return (
     <div>
       <BSNavbar bg="dark" variant="dark">
         <BSNavbar.Brand>{t('Expenses')}</BSNavbar.Brand>
+
+        <BSNav>
+          <BSNavDropdown title={t('Select Language')}>
+            <BSNavDropdown.Item href="/" as={BSDropdownItemLink}>
+              EN
+            </BSNavDropdown.Item>
+            <BSNavDropdown.Item href="/de" as={BSDropdownItemLink}>
+              DE
+            </BSNavDropdown.Item>
+          </BSNavDropdown>
+        </BSNav>
 
         <BSOverlayTrigger
           placement="bottom"
